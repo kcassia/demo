7c9d7c0c7aff91d911c5b589806e6facc08acf59
kcassia <kcassia@nate.com>
2021-04-09 23:05:31 +0900

    Initial CRUD

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import org.springframework.data.mongodb.repository.DeleteQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
