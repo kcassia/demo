cf0107294d84d285e1c673320aabd103fed69c96
kcassia <kcassia@nate.com>
2021-04-16 22:39:21 +0900

    Add files via upload
    Apply HTTP ResponseEntity

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -25,58 +28,61 @@ public class UserController {
 
     // @RequestMapping(value = "", method = RequestMethod.GET)
     @GetMapping("")
-    public List<User> getAllUsers() {
+    public ResponseEntity getAllUsers() {
         LOG.info("Getting all users.");
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        return (users.size() > 0) ? ResponseEntity.ok(users) : ResponseEntity.notFound().build();
     }
 
     // @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
     @GetMapping("/{userId}")
-    public User getUser(@PathVariable String userId) {
+    public ResponseEntity getUser(@PathVariable String userId) {
         LOG.info("Getting user with ID: {}.", userId);
-        return userRepository.findById(userId).orElse(null);
+        User user = userRepository.findById(userId).orElse(null);
+        return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
     // @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
     @DeleteMapping("/{userId}")
-    public void deleteUser(@PathVariable String userId) {
+    public ResponseEntity deleteUser(@PathVariable String userId) {
         LOG.info("Deleting user with ID: {}.", userId);
         userRepository.deleteById(userId);
+        return ResponseEntity.noContent().build();
     }
 
     // @RequestMapping(value = "", method = RequestMethod.POST)
     @PostMapping("")
-    public User addNewUsers(@RequestBody User user)
+    public ResponseEntity addNewUsers(@RequestBody User user)
     {
         LOG.info("Creating user with ID: {}.", user.getUserId());
-        return userRepository.save(user);
+        return ResponseEntity.created(URI.create("/" + user.getUserId())).body(userRepository.save(user));
     }
 
     // @RequestMapping(value = "/{userId}", method = RequestMethod.PUT)
     @PutMapping("/{userId}")
-    public User setUser(@PathVariable String userId, @RequestBody User user)
+    public ResponseEntity setUser(@PathVariable String userId, @RequestBody User user)
     {
         if (userId.equals(user.getUserId()))
         {
             LOG.info("Updating user by PUT with ID: {}.", user.getUserId());
-            return userRepository.save(user);
+            return (userRepository.existsById(userId)) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
         }
         else
         {
             LOG.info("Updating user by PUT fail due to mismatch {} vs {}", userId, user.getUserId());
-            return null;
+            return ResponseEntity.badRequest().build();
         }
     }
 
     // @RequestMapping(value = "/{userId}", method = RequestMethod.PATCH)
     @PatchMapping("/{userId}")
-    public User setUser(@PathVariable String userId, @RequestBody Map<String, Object> update)
+    public ResponseEntity setUser(@PathVariable String userId, @RequestBody Map<String, Object> update)
     {
         User user = userRepository.findById(userId).orElse(null);
         if (user == null)
         {
             LOG.info("User not found");
-            return null;
+            return ResponseEntity.notFound().build();
         }
 
         update.forEach(
@@ -101,52 +107,37 @@ public class UserController {
 
         LOG.info("Updating user by PATCH with ID: {}.", user.getUserId());
 
-        return userRepository.save(user);
+        return ResponseEntity.ok(userRepository.save(user));
     }
 
     // @RequestMapping(value = "/settings/{userId}", method = RequestMethod.GET)
     @GetMapping("/settings/{userId}")
-    public Object getAllUserSettings(@PathVariable String userId)
+    public ResponseEntity getAllUserSettings(@PathVariable String userId)
     {
         User user = userRepository.findById(userId).orElse(null);
-        if (user != null)
-        {
-            return user.getUserSettings();
-        }
-        else
-        {
-            return "User not found.";
-        }
+        return (user != null) ? ResponseEntity.ok(user.getUserSettings()) : ResponseEntity.notFound().build();
     }
 
     // @RequestMapping(value = "/settings/{userId}/{key}", method = RequestMethod.GET)
     @GetMapping("settings/{userId}/{key}")
-    public String getUserSetting(@PathVariable String userId, @PathVariable String key)
+    public ResponseEntity getUserSetting(@PathVariable String userId, @PathVariable String key)
     {
         User user = userRepository.findById(userId).orElse(null);
-        if (user != null)
-        {
-            return user.getUserSettings().get(key);
-        }
-        else
-        {
-            return "User not found.";
-        }
+        return (user != null) ? ResponseEntity.ok(user.getUserSettings().get(key)) : ResponseEntity.notFound().build();
     }
 
-    // @RequestMapping(value = "/settings/{userId}/{key}/{value}", method = RequestMethod.GET)
-    @GetMapping("/settings/{userId}/{key}/{value}")
-    public String addUserSetting(@PathVariable String userId, @PathVariable String key, @PathVariable String value)
+    // @RequestMapping(value = "/settings/{userId}/{key}/{value}", method = RequestMethod.POST)
+    @PutMapping("/settings/{userId}/{key}/{value}")
+    public ResponseEntity addUserSetting(@PathVariable String userId, @PathVariable String key, @PathVariable String value)
     {
         User user = userRepository.findById(userId).orElse(null);
         if (user != null)
         {
             user.getUserSettings().put(key, value);
-            userRepository.save(user);
-            return "Key added";
+            return ResponseEntity.ok(userRepository.save(user));
         }
         else {
-            return "User not found.";
+            return ResponseEntity.notFound().build();
         }
     }
 }
